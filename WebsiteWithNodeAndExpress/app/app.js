a65001220f675c2dc8d186f7c5c1f51863971499
lZzozZl <r.yordanov89@gmail.com>
2017-12-19 13:47:05 +0200

    Preparing for Routing

--- WebsiteWithNodeAndExpress/app/app.js
+++ WebsiteWithNodeAndExpress/app/app.js
@@ -1,12 +1,48 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
 
-app.get('/', function(req,res){
-  res.send('<h1>Zdrastiii');
-  var ip = req.ip;
-  console.log(ip);
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  res.send(`
+    <h1>Welcome!</h1>
+    <p>To R.Yordanov Website</p>
+  `);
+
+    // /** My hacks */
+    // var ip = req.ip;                          //getting user IP
+    // var browser = req.headers['user-agent'];  //getting user Browser
+    // console.log(ip, browser);                 //Print IP & Browser in console
 });
 
-var server = app.listen(3000, function(){
-  console.log('Listening on 3000');
+
+  app.get('/speakers', function(req,res){
+    var info = '';
+    dataFile.speakers.forEach(function(item) {
+      info += `
+        <li>
+          <h2>${item.name}</h2>
+          <p>${item.summary}</p>
+        </li>
+      `;
+    });
+    res.send(`
+      <h1>Roux Academy Meetups</h1>
+      ${info}
+    `);
+  });
+
+  app.get('/speakers/:speakerid', function(req,res){
+
+    var speaker = dataFile.speakers[req.params.speakerid];
+    res.send(`
+      <h1>${speaker.title}</h1>
+      <h2>with ${speaker.name}</h2>
+      <p>${speaker.summary}</p>
+    `);
+});
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Listening on ' + app.get('port'));
 });
